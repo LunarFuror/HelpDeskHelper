e5547cbf55f1b02ff1f9f6ad754a226a4fa01fa1
LunarFuror <lunar_furor@hotmail.com>
2014-05-20 08:42:34 -0500

    Changed sizes, clear on submit, confirm or error
    Size was too big toned it down, Now when creating the file it confirms
    being made, or shows an error. Fixed text area so it now wraps and
    extends correctly where as before it just kinda did w/e it wanted as far
    as you typing in it. Also info formats for easier reading.

--- HelpDeskHelper.java
+++ HelpDeskHelper.java
@@ -1,8 +1,3 @@
-/**
- * Author: Grayson Lorenz, 5/19/2014
- **/
-
-
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -14,8 +9,10 @@ import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class HelpDeskHelper extends JFrame{
 	//Fields
 	private JButton submitB;
@@ -33,7 +30,7 @@ public class HelpDeskHelper extends JFrame{
 	private Date date;
 	private PrintWriter writer;
 	//size of window
-	private static final int WIDTH=800,HEIGHT=600;
+	private static final int WIDTH=400,HEIGHT=300;
 	
 	//The setup and go
 	public HelpDeskHelper(){
@@ -46,8 +43,9 @@ public class HelpDeskHelper extends JFrame{
 		ipTF = new JTextField();
 		
 		//Description instantiation
-		descriptionTA = new JTextArea(100,1);
-		descriptionTA.setPreferredSize(new Dimension(600, 400));
+		descriptionTA = new JTextArea(1,1);
+		descriptionTA.setLineWrap(true);
+		descriptionTA.setWrapStyleWord(true);
 		descriptionTA.setAlignmentX(LEFT_ALIGNMENT);
 		
 		//panel instantiation
@@ -59,7 +57,7 @@ public class HelpDeskHelper extends JFrame{
 		ipP = new JPanel();
 		ipP.setLayout(new BoxLayout(ipP, BoxLayout.PAGE_AXIS));
 		descriptionP = new JScrollPane(descriptionTA);
-		descriptionP.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		descriptionP.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		phoneP = new JPanel(new BorderLayout());
 		
 		//The rest of the instantiation
@@ -105,9 +103,11 @@ public class HelpDeskHelper extends JFrame{
 		String name;
 		String theDate;
 		String nameBuffer;
+		String buffer;
 		public BHandler(){
 			name = "";
 			nameBuffer = "";
+			buffer = "";
 		}
 		
 		/**
@@ -121,9 +121,11 @@ public class HelpDeskHelper extends JFrame{
 			//reset name just to be safe
 			nameBuffer = "";
 			name = nameTF.getText();
+			buffer = "";
 			
 			//set up date for file name use
 			theDate = dateFormat.format(date) + "_";
+			
 			
 			//set up name buffer. Takes in the name and puts _ where the spaces are for file name use.
 			for(int i = 0; i < name.length(); i ++){
@@ -142,13 +144,31 @@ public class HelpDeskHelper extends JFrame{
 				writer.println("");
 				writer.println("IP ADDRESS: " + ipTF.getText() );
 				writer.println("");
-				writer.println("INFO: " + descriptionTA.getText() );
+				writer.print("INFO:\t");
+				buffer = descriptionTA.getText();
+				for(int i = 0; i < buffer.length(); i ++){
+					String letter = buffer.substring(i, i+1);
+					if(letter.compareTo("\n") == 0){
+						writer.println("");
+						writer.print("\t");
+					}
+					else{
+						writer.print(letter);
+					}
+				}
+				nameTF.setText("");
+				ipTF.setText("");
+				phoneTF.setText("");
+				descriptionTA.setText("");
 				writer.close();
+				JOptionPane.showMessageDialog(null, "File Created!", "Success!", 1);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "File not found :(", "Uh-oh!", ERROR);
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
-			}
+				JOptionPane.showMessageDialog(null, "Could not creat file! Unsupported Charset: UTF-8", "Uh-oh!", ERROR);
+			}	
 		}
 	}
 	
